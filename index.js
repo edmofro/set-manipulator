fdfe53bbafbb864aada75e040647175f33f42606
Edwin <edwin@sussol.net>
2016-07-19 17:05:15 +1200

    Export all set operation functions

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 import SetManipulator from './SetManipulator';
-export default new SetManipulator();
+const setManipulator = new SetManipulator();
+
+export default setManipulator;
+export function union(...args) { setManipulator.union(...args); }
+export function difference(...args) { setManipulator.difference(...args); }
+export function intersection(...args) { setManipulator.intersection(...args); }
+export function complement(...args) { setManipulator.complement(...args); }
+export function equals(...args) { setManipulator.equals(...args); }
